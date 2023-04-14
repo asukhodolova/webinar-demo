806bd6084c0aa5d4c744d2de1981ba9ff94c97a5
Anna Sukhodolova <asukhodolova@solvd.com>
2023-04-14 13:22:30 +0300

    Updated for demo

--- sample-cypress/cypress.config.js
+++ sample-cypress/cypress.config.js
@@ -1,20 +1,9 @@
 const { defineConfig } = require('cypress');
-const zbrPlugin = require('@zebrunner/javascript-agent-cypress/lib/plugin');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  reporter: '@zebrunner/javascript-agent-cypress',
-  reporterOptions: {
-    reportingServerHostname: 'https://yourworkspace.zebrunner.com/',
-    reportingServerAccessToken: 'yourtokenhere',
-    reportingProjectKey: 'DEF',
-    reportingRunDisplayName: 'Cypress Demo',
-    reportingRunEnvironment: 'LOCAL',
-    reportingRunBuild: '1.0-alpha',
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      zbrPlugin(on, config);
     },
   },
 });
--- sample-cypress/cypress/e2e/demo.cy.js
+++ sample-cypress/cypress/e2e/demo.cy.js
@@ -13,14 +13,4 @@ describe('Cypress Google search', () => {
         cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
     });
 
-    it('Should return first result that equals to search value', () => {
-        cy.visit(url).contains('Google');
-
-        console.log(`Performing Google search`);
-        cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
-
-        console.log(`Verify first search result equals to search value`);
-        cy.xpath("//*[@id='search']//a").should('have.text', searchValue);
-    });
-
 });
